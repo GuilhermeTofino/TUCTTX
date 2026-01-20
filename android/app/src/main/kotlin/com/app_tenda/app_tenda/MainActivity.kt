c70fd01c24dee42fb1b584af3ecf7ba11d49ced0
Guilherme Tofino <gpulcino4@gmail.com>
2026-01-20 18:01:21 -0300

    Reinicializando projeto do zero com flutter create

--- android/app/src/main/kotlin/com/app_tenda/app_tenda/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.app_tenda.app_tenda
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ app_tenda/android/app/src/main/kotlin/com/appTenda/app_tenda/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.appTenda.app_tenda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
